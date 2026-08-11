b9b1e084b325d82ee9c0c632e691eb9a31180f3e
JesusManriquez <jesusmanriquez56@aragon.unam.mx>
2026-08-11 10:29:23 -0600

    Se agrega version inicial de pagina de proyectos

---
+++ src/data/portfolioProjects.ts
@@ -0,0 +1,62 @@
+export interface PortfolioProject {
+  name: string;
+  description?: string;
+  location?: string;
+  year?: string;
+
+  images: string[];
+}
+
+export const portfolioProjects: PortfolioProject[] = [
+  {
+    name: "Obra 1",
+
+    description:
+      "*Descripción de los trabajos realizados en esta obra*",
+
+    location: "Ciudad de México / Estado de México",
+
+    year: "202X",
+
+    images: [
+      "/images/portfolio/obra01/01.jpg",
+      "/images/portfolio/obra01/02.jpg",
+      "/images/portfolio/obra01/03.jpg",
+      "/images/portfolio/obra01/04.jpg",
+    ],
+  },
+
+  {
+    name: "Obra 2",
+
+    description:
+      "*Descripción de los trabajos realizados en esta obra*",
+
+    location: "Ciudad de México / Estado de México",
+
+    year: "202X",
+
+    images: [
+      "/images/portfolio/obra02/01.jpg",
+      "/images/portfolio/obra02/02.jpg",
+      "/images/portfolio/obra02/03.jpg",
+    ],
+  },
+
+  {
+    name: "Obra 3",
+
+    description:
+      "*Descripción de los trabajos realizados en esta obra*",
+
+    location: "Ciudad de México / Estado de México",
+
+    year: "202X",
+
+    images: [
+      "/images/portfolio/obra03/01.jpg",
+      "/images/portfolio/obra03/02.jpg",
+      "/images/portfolio/obra03/03.jpg",
+    ],
+  },
+];
