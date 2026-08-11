536acb6b837ea4bc3c56072f062f4d9bdfe1edc9
JesusManriquez <jesusmanriquez56@aragon.unam.mx>
2026-08-11 10:04:44 -0600

    Refactorizacion de datos, cambio de nombre por featuredProjects

---
+++ src/data/featuredProjects.ts
@@ -0,0 +1,26 @@
+export const featuredProjects = [
+  {
+    title: "Agencia Automotriz",
+    category: "Comercial",
+    type: "Construcción",
+    description:
+      "Desarrollo y ejecución de proyecto para agencia automotriz.",
+    image: "/images/Projects/agencia-automotriz.jpg",
+  },
+  {
+    title: "Proyecto de remodelación",
+    category: "Remodelación",
+    type: "Adecuación de espacios",
+    description:
+      "Transformación integral de espacios existentes.",
+    image: "/images/Projects/remodelacion.jpg",
+  },
+  {
+    title: "Proyecto corporativo",
+    category: "Comercial",
+    type: "Construcción",
+    description:
+      "Desarrollo de espacios corporativos funcionales y modernos.",
+    image: "/images/Projects/proyecto-corporativo.jpg",
+  },
+];
