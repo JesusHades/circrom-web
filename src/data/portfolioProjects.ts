6d43906e5261032a8708021ce2886c267ff5de6f
JesusManriquez <jesusmanriquez56@aragon.unam.mx>
2026-08-11 17:13:46 -0600

    Se agregan imagenes de contenido de proyectos

--- src/data/portfolioProjects.ts
+++ src/data/portfolioProjects.ts
@@ -9,57 +9,87 @@ export interface PortfolioProject {
 
 export const portfolioProjects: PortfolioProject[] = [
   {
-    name: "Obra 1",
+    name: "BYD Lomas Verdes",
 
     description:
       "*Descripción de los trabajos realizados en esta obra*",
 
-    location: "Ciudad de México / Estado de México",
+    location: "Naucalpan, Estado de México",
 
-    year: "202X",
+    year: "2024",
 
     images: [
-      "/images/portfolio/obra01/01.jpg",
-      "/images/portfolio/obra01/02.jpg",
-      "/images/portfolio/obra01/03.jpg",
-      "/images/portfolio/obra01/04.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/01.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/02.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/03.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/04.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/05.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/06.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/07.jpg",
+      "/images/portfolio/obra01-BYDLomasVerdes/08.jpg",
     ],
   },
 
   {
-    name: "Obra 2",
+    name: "GWM Ecatepec",
 
     description:
       "*Descripción de los trabajos realizados en esta obra*",
 
-    location: "Ciudad de México / Estado de México",
+    location: "Ecatepec, Estado de México",
 
-    year: "202X",
+    year: "2025",
 
     images: [
-      "/images/portfolio/obra02/01.jpg",
-      "/images/portfolio/obra02/02.jpg",
-      "/images/portfolio/obra02/03.jpg",
-      "/images/portfolio/obra02/04.jpg",
-      "/images/portfolio/obra02/05.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/01.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/02.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/03.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/04.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/05.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/06.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/07.jpg",
+      "/images/portfolio/obra02-GWMEcatepec/08.jpg",
     ],
   },
 
   {
-    name: "Obra 3",
+    name: "Kasa Coacalco",
 
     description:
       "*Descripción de los trabajos realizados en esta obra*",
 
-    location: "Ciudad de México / Estado de México",
+    location: "Coacalco, Estado de México",
 
-    year: "202X",
+    year: "2025",
 
     images: [
-      "/images/portfolio/obra03/01.jpg",
-      "/images/portfolio/obra03/02.jpg",
-      "/images/portfolio/obra03/03.jpg",
-      "/images/portfolio/obra03/04.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/01.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/02.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/03.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/04.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/05.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/06.jpg",
+      "/images/portfolio/obra03-KasaCoacalco/07.jpg",
+    ],
+  },
+
+  {
+    name: "Geely Santa Monica Tlalnepantla",
+
+    description:
+      "*Descripción de los trabajos realizados en esta obra*",
+
+    location: "Tlalnepantla, Estado de México",
+
+    year: "2026",
+
+    images: [
+      "/images/portfolio/obra04-GeelyStaMonica/01.jpg",
+      "/images/portfolio/obra04-GeelyStaMonica/02.jpg",
+      "/images/portfolio/obra04-GeelyStaMonica/03.jpg",
+      "/images/portfolio/obra04-GeelyStaMonica/04.jpg",
+      "/images/portfolio/obra04-GeelyStaMonica/05.jpg",
+      "/images/portfolio/obra04-GeelyStaMonica/06.jpg",
     ],
   },
 ];
